18d612e8634fec016cce5de4d23d3ae75ce731d2
Piotr Czajka <piotr_czajka@outlook.com>
2016-11-07 02:21:37 +0100

    Added docstrings to ftp.py

--- ftp.py
+++ ftp.py
@@ -5,6 +5,14 @@ import os
 ftp = FTP(host=config.get('ftp_host'), user=config.get('ftp_user'), passwd=config.get('ftp_password'))
 
 def upload_file(filepath):
+    """Function, which uploads one file to a configured ftp server
+
+    Args:
+    string : filepath - a path to file to send
+
+    Returns:
+    string: link - a link to uploaded file, with ftp:// prefix"""
+
     path_to_file_folder = os.path.dirname(filepath)
     os.chdir(path_to_file_folder)
     filename = os.path.basename(filepath)
@@ -15,4 +23,6 @@ def upload_file(filepath):
         pass
 
     ftp.storbinary("STOR {folder}/{filename}".format(filename = filename, folder = folder), open(filename, "rb"))
-    return '{host}/{folder}/{filename}'.format(host = config.get('ftp_host'), filename = filename, folder = folder)
+
+    link = '{host}/{folder}/{filename}'.format(host = config.get('ftp_host'), filename = filename, folder = folder)
+    return link
